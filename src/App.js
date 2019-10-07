a596fda5868997a104bd74f84bd5b84081a499a8
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-10-07 16:04:11 -0500

    added more scoring buttons and a reset button. with some styling changes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">Good</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
@@ -20,7 +20,7 @@ function App() {
           </div>
           <div className="timer">00:03</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name">Bad</h2>
             <div className="away__score">{awayCount}</div>
           </div>
         </div>
@@ -29,12 +29,21 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => setHCount(homeCount + 7)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal"onClick={() => setHCount(homeCount + 3)}>Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={() => setHCount(homeCount + 6)}>Good Touchdown</button>
+          <button className="homeButtons__touchdown" onClick={() => setHCount(homeCount + 1)}>Good PAT</button>
+          <button className="homeButtons__touchdown" onClick={() => setHCount(homeCount + 2)}>Good 2 point conversion</button>
+          <button className="homeButtons__touchdown" onClick={() => setHCount(homeCount + 3)}>Good Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={() => setHCount(homeCount + 2)}>Good Safety</button>
+        </div>
+        <div>
+        <button className="homeButtons__touchdown" onClick={() => {setHCount(0); setACount(0);}}>Reset</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => setACount(awayCount + 7)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={() => setACount(awayCount + 3)}>Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={() => setACount(awayCount + 6)}>Bad Touchdown</button>
+          <button className="awayButtons__touchdown" onClick={() => setACount(awayCount + 1)}>Bad PAT</button>
+          <button className="awayButtons__touchdown" onClick={() => setACount(awayCount + 2)}>Bad 2 point conversion</button>
+          <button className="awayButtons__touchdown" onClick={() => setACount(awayCount + 3)}>Bad Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={() => setACount(awayCount + 2)}>Bad Safety</button>
         </div>
       </section>
     </div>
